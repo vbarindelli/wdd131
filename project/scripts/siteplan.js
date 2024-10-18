94a6da06e80baf6aafc9967ef1c9160191659f58
vbarindelli <hae21002@byu.edu>
2024-10-19 01:17:48 +0200

    views

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,4 +1,17 @@
+const mainnav = document.querySelector('.navigation');
+const hamburger = document.querySelector('#menu');
+
 hamburger.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hamburger.classList.toggle('show');
 })
+
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `@ <span class="highlight">${today.getFullYear()}</span> Virginia Haedo - Malaga, Spain`;
+
+
+
+const lastModified = document.querySelector("#lastModified");
+let modified = new Date(document.lastModified);
+lastModified.innerHTML = `Last Modification: ${modified}</span>`;
